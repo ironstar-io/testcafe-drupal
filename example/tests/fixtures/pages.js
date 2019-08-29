019390bae78506d6c3b7060e4ef3ad001d987fe1
Ben Gunn <ben@technocrat.com.au>
2019-08-30 09:59:46 +1000

    Used shortened form of arrow function in page accessiblity test

--- example/tests/fixtures/pages.js
+++ example/tests/fixtures/pages.js
@@ -9,8 +9,6 @@ test.requestHooks(logger).page(baseUrl)("Homepage is accessible", async t => {
   // Ensure that the response has been received and that its status code is
   // 200.
   await t
-    .expect(logger.contains(record => {
-      return record.response.statusCode === 200
-    }))
+    .expect(logger.contains(record => record.response.statusCode === 200))
     .ok("The page request returned a status code of " + logger.requests[0].response.statusCode);
 });
