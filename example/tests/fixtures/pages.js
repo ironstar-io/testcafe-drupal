d338af35c0efb536bca0160648a98c6c2b1be244
Ben Gunn <ben@technocrat.com.au>
2019-08-30 09:56:29 +1000

    Included response status code on page access assertion failure

--- example/tests/fixtures/pages.js
+++ example/tests/fixtures/pages.js
@@ -9,6 +9,8 @@ test.requestHooks(logger).page(baseUrl)("Homepage is accessible", async t => {
   // Ensure that the response has been received and that its status code is
   // 200.
   await t
-    .expect(logger.contains(record => record.response.statusCode === 200))
-    .ok();
+    .expect(logger.contains(record => {
+      return record.response.statusCode === 200
+    }))
+    .ok("The page request returned a status code of " + logger.requests[0].response.statusCode);
 });
