dedc25ea3d1c92ed94580bdfae8b65ba598b20bf
Ben Gunn <ben@technocrat.com.au>
2019-10-16 14:59:55 +1100

    [9] Created prototype TestRunner class

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const message = require("./src/message");
 const node = require("./src/node");
 const system = require("./src/system");
 const users = require("./src/users");
+const testRunner = require("./src/testRunner");
 
 module.exports = {
   ...field,
   ...message,
   ...node,
   ...system,
-  ...users
+  ...users,
+  ...testRunner
 };
